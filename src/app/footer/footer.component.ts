f49dac5e78175704f54fbbd5c632212620ad0bae
Gabriel de Barros <gabrielbpht01@gmail.com>
2024-06-14 13:41:42 -0300

    feat(quality_code): instala prettier e ESLint

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
   standalone: true,
-  imports: [
-    MatCardModule,
-  ]
+  imports: [MatCardModule]
 })
-export class FooterComponent {
-  
-}
+export class FooterComponent {}
