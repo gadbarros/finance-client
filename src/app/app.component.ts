b5a7788a1b79e8cca7fab6c55d53e68995a5d842
Gabriel de Barros <gabrielbpht01@gmail.com>
2025-01-22 01:03:41 -0300

    ajusta estrutura do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AsyncPipe } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { Observable } from 'rxjs';
+import { BreakpointService } from './shared/service/BreakpointService';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    //RouterOutlet,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    AsyncPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'finance-client';
+  isHandset$: Observable<boolean>;
+
+  constructor(private breakpointService: BreakpointService) {
+    this.isHandset$ = this.breakpointService.isHandset$;
+  }
 }
